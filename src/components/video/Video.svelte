8ec829d05e20f7e8960f12649554974b23cd95a3
Nicolas Tapia <ntapia.dev@gmail.com>
2023-09-23 22:34:12 +0200

    Ajout de la vidéo

---
+++ src/components/video/Video.svelte
@@ -0,0 +1,15 @@
+<video autoplay loop muted playsinline>
+	<source src="video.mp4" type="video/mp4" />
+</video>
+
+<style lang="scss">
+	video {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		z-index: -999;
+	}
+</style>
